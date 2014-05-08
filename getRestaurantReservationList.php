55b48f344d4df87a328a9979aff666a98b9ed4b2
Tiitan <florian.n@hotmail.fr>
2014-05-08 16:16:54 +0800

    getRestaurantReservationList ajout paramétre state

--- getRestaurantReservationList.php
+++ getRestaurantReservationList.php
@@ -3,7 +3,7 @@
 include ('includes/sqlConnection.php');
 include ('includes/utils.php');
 
-$params = GetParameters('dateFrom', 'dateTo', 'token');
+$params = GetParameters('dateFrom', 'dateTo', 'state', 'token');
 if ($params != null && checkParameters($params))
 {
 	$dbc = ConnectToDataBase();
@@ -21,8 +21,8 @@ function process($params, $session)
 	$dbc = ConnectToDataBase();
 	// $a = array('idRestaurant' => $session['idUser'], 'dateFrom' => $params['dateFrom'], 'dateTo' => $params['dateTo']);
 	// echo '<pre>'; print_r($a); echo '</pre>';
-	$request = $dbc->prepare("SELECT Reservation.idReservation， Reservation.idRestaurant, Reservation.idUser, Reservation.personNumber, Reservation.date, Reservation.state, Reservation.email,  User.name FROM Reservation INNER JOIN User ON User.idUser = Reservation.idUser WHERE idRestaurant = :idRestaurant AND date > CAST(:dateFrom AS time) AND date < CAST(:dateTo AS time)");
-	$request->execute(array('idRestaurant' => $session['idUser'], 'dateFrom' => $params['dateFrom'], 'dateTo' => $params['dateTo']));
+	$request = $dbc->prepare("SELECT Reservation.idReservation， Reservation.idRestaurant, Reservation.idUser, Reservation.personNumber, Reservation.date, Reservation.state, Reservation.email,  User.name FROM Reservation INNER JOIN User ON User.idUser = Reservation.idUser WHERE Reservation.idRestaurant = :idRestaurant AND Reservation.state = :state AND Reservation.date > CAST(:dateFrom AS time) AND Reservation.date < CAST(:dateTo AS time)");
+	$request->execute(array('idRestaurant' => $session['idUser'], 'dateFrom' => $params['dateFrom'], 'dateTo' => $params['dateTo'], 'state' => $params['state']));
 	
 	$reservationList = array();
 	while($result = $request->fetch()) 
