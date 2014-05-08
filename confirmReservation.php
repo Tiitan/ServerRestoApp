032c15ebe0db38ae603a51072f4fc9cb2cc8040a
Alexandre BRISPOT <alexandre.brispot@epitech.eu>
2014-05-08 17:31:33 +0800

    confirmReservation fix

--- confirmReservation.php
+++ confirmReservation.php
@@ -7,7 +7,7 @@ $params = GetParameters('idReservation', 'token', 'state');
 if ($params != null && checkParameters($params))
 {
 	$dbc = ConnectToDataBase();
-	$session = GetSession(params[token], $dbc);
+	$session = GetSession($params[token], $dbc);
 	if ($session != null && isLoggedAsRestaurant($session))
 		process($params, $session);
 }
@@ -19,17 +19,17 @@ if ($params != null && checkParameters($params))
 function process($params, $session)
 {
 	$request = $dbc->prepare("Update Reservation SET state=:state WHERE idReservation=:idReservation AND idRestaurant=:idRestaurant");
-	$request->execute(array('state' => params['state'], 'idReservation' => params['idReservation'], 'idRestaurant' => session['idUser']));
+	$request->execute(array('state' => $params['state'], 'idReservation' => $params['idReservation'], 'idRestaurant' => $session['idUser']));
 	$request->closeCursor();
 	
 	//TODO: check that updated rows number = 1
 	
-	echo '{"islog":"true", "text":""}';
+	echo '{"islog":true, "text":""}';
 }
 
 function checkParameters($params)
 {
-	if (state == 'conf' || state == 'rej')
+	if ($params['state'] == 'conf' || $params['state'] == 'rej')
 		return true;
 	else
 		PrintError('Invalid parameters.');
