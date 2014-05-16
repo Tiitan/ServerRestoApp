edf099f796e3a29a7995adb54940c6fa60b92271
Tiitan <florian.n@hotmail.fr>
2014-05-16 14:22:11 +0800

    update resto info, test update sucess

--- updateRestaurantInfo.php
+++ updateRestaurantInfo.php
@@ -22,17 +22,18 @@ function process($params, $session, $dbc)
 	//TODO: check that state is confirmed
 	
 	$request = $dbc->prepare("Update Restaurant SET address=:address, tel=:phone, name=:name, description=:description WHERE idRestaurant=:idRestaurant");
-	$request->execute(array('idRestaurant' => $session['idUser'], 
-							'address' => isset($_GET[$params['address']]) ? $params['address'] : '', 
-							'phone' => isset($_GET[$params['phone']]) ? $params['phone'] : '', 
-							'name' => isset($_GET[$params['name']]) ? $params['name'] : '', 
-							'description' => isset($_GET[$params['description']]) ? $params['description'] : ''));
-							//TODO do not erase data
+	$result = $request->execute(array('idRestaurant' => $session['idUser'], 
+										'address' => isset($_GET[$params['address']]) ? $params['address'] : '', 
+										'phone' => isset($_GET[$params['phone']]) ? $params['phone'] : '', 
+										'name' => isset($_GET[$params['name']]) ? $params['name'] : '', 
+										'description' => isset($_GET[$params['description']]) ? $params['description'] : ''));
+										//TODO do not erase data
 	$request->closeCursor();
 	
-	//TODO: check that updated rows number = 1
-	
-	echo '{"islog":"true", "text":""}';
+	if ($result->rowCount() == 1)
+		echo '{"islog":"true", "text":""}';
+	else
+		echo '{"islog":"false", "text":"Error while updating"}';
 }
 
 function checkParameters($params)
